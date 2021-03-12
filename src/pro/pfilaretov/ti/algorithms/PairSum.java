79c563de458146df8ba450fafc1dac8836b96541
Petr Filaretov <petr.filaretov@gmail.com>
2021-03-12 19:37:59 +0300

    algorithmic task - one more implementation

--- src/pro/pfilaretov/ti/algorithms/PairSum.java
+++ src/pro/pfilaretov/ti/algorithms/PairSum.java
@@ -1,29 +1,32 @@
 package pro.pfilaretov.ti.algorithms;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
- * Write a function that takes two args: a list of integers ("list") and a number ("targetSum").
- * The function should find and return all pairs such that their sum equals to "targetSum".
- * For instance,
- * list = [1, 2, 3, 4, 5]
- * targetSum = 6
- * result: [[1, 5], [2, 4]]
- *
- * Solution:
- * - brut force -> O(n^2)
- * - Use Hash Map/Set -> O(n)
+ * Write a function that takes two args: a list of integers ("list") and a number ("targetSum"). The function should
+ * find and return all pairs such that their sum equals to "targetSum".
+ * <p>For instance,</p>
+ * <p>list = [1, 2, 3, 4, 5]</p>
+ * <p>targetSum = 6</p>
+ * <p>result: [[1, 5], [2, 4]]</p>
+ * <p/>
+ * <p>Solution:</p>
+ * <p>- brut force -> O(n^2)</p>
+ * <p>- Use Hash Map/Set -> O(n)</p>
  */
 public class PairSum {
 
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+        List<Integer> list = Stream.iterate(1, i -> i + 1).limit(20).collect(Collectors.toList());
+        Collections.shuffle(list);
         int targetSum = 12;
 
         System.out.println("list: " + list);
@@ -31,7 +34,8 @@ public class PairSum {
 
 //        List<Pair> pairs = findPairsBrutForce(list, targetSum);
 //        List<Pair> pairs = findPairs(list, targetSum);
-        List<Pair> pairs = findPairsHashMap(list, targetSum);
+//        List<Pair> pairs = findPairsHashMap(list, targetSum);
+        List<Pair> pairs = findPairsSet(list, targetSum);
 
         System.out.println("result: " + pairs);
     }
@@ -80,7 +84,6 @@ public class PairSum {
 
     private static List<Pair> findPairsHashMap(List<Integer> list, int targetSum) {
         List<Pair> result = new ArrayList<>();
-
         Map<Integer, Integer> map = new HashMap<>();
 
         for (Integer i : list) {
@@ -89,6 +92,21 @@ public class PairSum {
                 result.add(new Pair(i, j));
             } else {
                 map.put(i, i);
+            }
+        }
+
+        return result;
+    }
+
+    private static List<Pair> findPairsSet(List<Integer> list, int targetSum) {
+        List<Pair> result = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+
+        for (Integer i : list) {
+            if (set.contains(targetSum - i)) {
+                result.add(new Pair(i, targetSum - i));
+            } else {
+                set.add(i);
             }
         }
 
